927bb4f659f50d6caa8c8a92a891bffdb70817bb
emilgalimov <41144687+emilgalimov@users.noreply.github.com>
2022-04-14 23:30:04 +0300

    [feat] anagram passed

--- anagram/anagram.go
+++ anagram/anagram.go
@@ -1,5 +1,36 @@
 package anagram
 
+import "strings"
+
 func FindAnagrams(dictionary []string, word string) (result []string) {
-	return []string{}
+
+	length := len(strings.ReplaceAll(word, " ", ""))
+	result = []string{}
+	word = strings.ToLower(word)
+	for _, dictionaryWord := range dictionary {
+		tmpDictionaryWord := strings.ToLower(dictionaryWord)
+		if tmpDictionaryWord == word {
+			continue
+		}
+		if length == len(strings.ReplaceAll(tmpDictionaryWord, " ", "")) && 0 < len(strings.ReplaceAll(tmpDictionaryWord, " ", "")) {
+			if isSameLetters(word, tmpDictionaryWord) {
+				result = append(result, dictionaryWord)
+			}
+		}
+	}
+
+	return result
+}
+
+func isSameLetters(word string, dictionaryWord string) bool {
+	processedWord := strings.ReplaceAll(strings.ToLower(word), " ", "")
+	processedDictionaryWord := strings.ReplaceAll(strings.ToLower(dictionaryWord), " ", "")
+	for _, c := range word {
+		processedWord = strings.ReplaceAll(processedWord, string(c), "")
+		processedDictionaryWord = strings.ReplaceAll(processedDictionaryWord, string(c), "")
+		if len(processedWord) != len(processedDictionaryWord) {
+			return false
+		}
+	}
+	return true
 }
