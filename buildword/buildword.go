45a81079490f25fcf7efe36c7d98557dd15bec89
emilgalimov <41144687+emilgalimov@users.noreply.github.com>
2022-04-17 17:57:53 +0300

    [feat] buildword passed

--- buildword/buildword.go
+++ buildword/buildword.go
@@ -1,5 +1,51 @@
 package buildword
 
+import (
+	"errors"
+	"strings"
+)
+
 func BuildWord(word string, fragments []string) int {
-	return 0
+
+	variants, err := getPossibleVariants(word, fragments)
+
+	if err != nil {
+		return 0
+	}
+
+	min := 0
+	for _, variant := range variants {
+		if min == 0 || min > len(variant) {
+			min = len(variant)
+		}
+	}
+
+	return min
+}
+
+func getPossibleVariants(word string, fragments []string) ([][]string, error) {
+
+	var returnPossibilities [][]string
+	for _, fragment := range fragments {
+		if 0 == strings.Index(word, fragment) {
+			afterTrim := strings.TrimPrefix(word, fragment)
+			if "" == afterTrim {
+				returnPossibilities = append(returnPossibilities, []string{fragment})
+				continue
+			}
+			possibilities, err := getPossibleVariants(afterTrim, fragments)
+			if err != nil {
+				continue
+			}
+			for _, possibility := range possibilities {
+				returnPossibilities = append(returnPossibilities, append(possibility, fragment))
+			}
+		}
+	}
+
+	if nil == returnPossibilities {
+		return nil, errors.New("not possible variant")
+	}
+
+	return returnPossibilities, nil
 }
