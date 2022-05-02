767e49e56700e3ef265749ff2b65df2d1c278ca1
emilgalimov <41144687+emilgalimov@users.noreply.github.com>
2022-05-02 17:39:07 +0300

    [feat] missingnumbers passed

--- missingnumbers/missingnumbers.go
+++ missingnumbers/missingnumbers.go
@@ -1,5 +1,25 @@
 package missingnumbers
 
 func Missing(numbers []int) []int {
-	return []int{}
+	miss := make([]int, 0, 2)
+	i := 1
+	for {
+		if !contains(numbers, i) {
+			miss = append(miss, i)
+			if len(miss) == 2 {
+				break
+			}
+		}
+		i++
+	}
+	return miss
+}
+
+func contains(s []int, e int) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
