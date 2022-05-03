31b4af311347c47f662d9f0d07dc09524f8736f1
emilgalimov <41144687+emilgalimov@users.noreply.github.com>
2022-05-03 20:16:36 +0300

    [feat] secretmessage passed

--- secretmessage/secretmessage.go
+++ secretmessage/secretmessage.go
@@ -1,6 +1,49 @@
 package secretmessage
 
+import (
+	"sort"
+)
+
+type letter struct {
+	ch    rune
+	count int
+}
+
 // Decode func
 func Decode(encoded string) string {
-	return ""
+
+	count := make(map[rune]letter)
+
+	//count items
+	for _, char := range encoded {
+		item := count[char]
+
+		item.ch = char
+		item.count++
+
+		count[char] = item
+	}
+
+	//sort items
+	var sortedCount []letter
+
+	for _, v := range count {
+		sortedCount = append(sortedCount, v)
+	}
+
+	sort.Slice(sortedCount, func(i, j int) bool {
+		return sortedCount[i].count > sortedCount[j].count
+	})
+
+	//drop characters
+	var resultString string
+
+	for _, v := range sortedCount {
+		if v.ch == '_' {
+			break
+		}
+		resultString += string(v.ch)
+	}
+
+	return resultString
 }
