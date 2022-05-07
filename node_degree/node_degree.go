8d13bf6f772398ce6c2b410a391df0aeaa51c46f
emilgalimov <41144687+emilgalimov@users.noreply.github.com>
2022-05-08 00:32:50 +0300

    [feat] node_degree passed

--- node_degree/node_degree.go
+++ node_degree/node_degree.go
@@ -1,7 +1,23 @@
 package nodedegree
 
+import (
+	"fmt"
+)
+
 // Degree func
 func Degree(nodes int, graph [][2]int, node int) (int, error) {
-	return 0, nil
-}
+	if node > nodes {
+		return 0, fmt.Errorf("node %v not found in the graph", node)
+	}
 
+	var count int
+	for _, i := range graph {
+		if i[0] == node && i[1] == node {
+			count += 2
+		}
+		if i[0] == node || i[1] == node {
+			count++
+		}
+	}
+	return count, nil
+}
