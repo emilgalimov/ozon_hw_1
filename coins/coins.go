59fb118478167c1e91fa566a83bf143ead7a18cd
emilgalimov <41144687+emilgalimov@users.noreply.github.com>
2022-05-07 17:43:11 +0300

    [feat] coins passed

--- coins/coins.go
+++ coins/coins.go
@@ -1,6 +1,34 @@
 package coins
 
-
 func Piles(n int) int {
-	return 0
+	return piles(n, n)
+}
+
+var m = make(map[int]map[int]int)
+
+func piles(n, k int) int {
+
+	if n == 0 && k == 0 {
+		return 1
+	}
+
+	if k == 0 {
+		return 0
+	}
+
+	if _, nok := m[n]; !nok {
+		m[n] = make(map[int]int)
+	}
+
+	if v := m[n][k]; v != 0 {
+		return v
+	}
+
+	if k > n {
+		m[n][k] = piles(n, n)
+		return m[n][k]
+	}
+
+	m[n][k] = piles(n, k-1) + piles(n-k, k)
+	return m[n][k]
 }
